7d44d6edc3000fe62b64579b9bac117447042c72
Michael Charland <mike_ch_1@hotmail.com>
2019-09-13 12:29:46 -0400

    original commit

---
+++ Branching2019/Branching2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Branching2019
+//
+//  Created by Michael Charland on 2019-09-13.
+//  Copyright © 2019 Michael Charland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
